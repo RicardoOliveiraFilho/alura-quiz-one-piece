92e995b3879067727bd8f379fffc000bbdcb1fe5
Riardo Oliveira <ricardo.oliveira1307@gmail.com>
2021-01-30 20:00:03 -0300

    Terceiro commit do projeto Alura Quiz - One Piece!

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,19 @@
 /* eslint-disable react/jsx-closing-bracket-location */
 import React, { useState } from 'react';
-import styled from 'styled-components';
 
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 import db from '../db.json';
 
+import Button from '../src/components/Button';
 import Footer from '../src/components/Footer';
 import GithubCorner from '../src/components/GithubCorner';
+import Input from '../src/components/Input';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
 import QuizLogo from '../src/components/QuizLogo';
 import Widget from '../src/components/Widget';
-
-const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 export default function Home() {
   const [name, setName] = useState('');
@@ -52,19 +43,15 @@ export default function Home() {
               console.log('Fazendo uma submissão por meio do react');
               // router manda para a próxima página.
             }}>
-              <input
-                // eslint-disable-next-line func-names
-                onChange={function (infosDoEvento) {
-                  // eslint-disable-next-line no-console
-                  console.log(infosDoEvento.target.value);
-                  setName(infosDoEvento.target.value);
-                }}
-                placeholder="Diz aí seu nome" />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar
-                {' '}
-                { name }
-              </button>
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Diz aí seu nome"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
