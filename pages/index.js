27c79fab9d8fee5bcbd7d74e5eb3c0509dbb2a60
Riardo Oliveira <ricardo.oliveira1307@gmail.com>
2021-01-30 12:20:00 -0300

    Primeiro commit do projeto Alura Quiz - One Piece!

---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+
+import db from '../db.json';
+
+import Footer from '../src/components/Footer';
+import GithubCorner from '../src/components/GithubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
+import Widget from '../src/components/Widget';
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+export default function Home() {
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+    
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+            <p>links virão aqui!!!</p>
+          </Widget.Content>
+        </Widget>
+    
+        <Footer />
+      </QuizContainer>
+      <GithubCorner projectUrl="https://github.com/RicardoOliveiraFilho/alura-quiz-one-piece" />
+    </QuizBackground>
+  );
+}
